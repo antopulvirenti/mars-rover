0fc04d67605946095cf1b6e2acdc20ca8b47f087
Anto Pulvirenti <antopulvirenti@hotmail.com>
2021-12-22 17:01:04 -0300

    changes the folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
     screens: {
       xs: "360px",
