2967eb0bea616490a358bd5462ca2f00bf7f6cef
Anto Pulvirenti <antopulvirenti@hotmail.com>
2021-12-22 12:06:38 -0300

    adds tailwind, zustand, classnames and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
-  ],
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
+    screens: {
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1366px",
+      "3xl": "1920px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
